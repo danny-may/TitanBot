32636e12b01acb0eaa3b77e49b038d05238bbda5
Titansmasher <dannyjmay97@gmail.com>
2017-05-14 12:00:31 +0100

    Improved about

--- TitanBot2Core/Commands/General/AboutCommand.cs
+++ TitanBot2Core/Commands/General/AboutCommand.cs
@@ -15,20 +15,33 @@ namespace TitanBot2.Commands.General
             Calls.AddNew(a => ShowAbout());
             Usage.Add("`{0}` - Shows some about text for me");
             Description = "A tiny command that just displays some helpful links :";
+            _specialThanks = new Dictionary<ulong, string>
+            {
+                {181829392027090954, "Awesome guy, helped out with the initial testing of titanbot" },
+                {196387723018240000, $"Kept pestering {Context?.Client.GetUser(135556895086870528).Username} for things to be added" },
+                {257960645725650954, "Did all the images for the equipment" },
+                {130084593515626496, "Made my avatar and makes other cool things" }
+            };
         }
+
+        private Dictionary<ulong, string> _specialThanks;
 
         public async Task ShowAbout()
         {
-            var message = $@"'Ello there :wave:, Im Titanbot! Im an open source discord bot dedicated to TT2 written in C# by Titansmasher.
-I can do a variety of things, all of which are better documented in my `{Context.Prefix}help` command!
-
-Im always going to be taking suggestions, which can either be DM'd to Titansmasher (He has a terrible memory though) Or sent in the #suggestions channel on my support server!
-
-Public repository: <https://github.com/Titansmasher/TitanBot2>
-Support server: <https://discord.gg/abQPtHu>
-
-I also took heavy inspiration from a fellow bot, Blargbot, who was created by Stupid Cat. :heart:
-You can find out more about blargbot on its website: <https://blargbot.xyz/>";
+            var message = $"'Ello there :wave:, Im Titanbot! Im an open source discord bot dedicated to TT2 written in C# by {Context.Client.GetUser(135556895086870528).Username}.\n" +
+                          $"I can do a variety of things, all of which are better documented in my `{Context.Prefix}help` command!\n\n" +
+                          $"Im always going to be taking suggestions, which can either be DM'd to {Context.Client.GetUser(135556895086870528).Username} (He has a terrible memory though) Or sent in the #suggestions channel on my support server!\n\n" +
+                           "Public repository: <https://github.com/Titansmasher/TitanBot2>\n" +
+                           "Support server: <https://discord.gg/abQPtHu>\n\n" +
+                           ":sparkles: **Special thanks to** :sparkles:\n";
+            var count = 1;
+            foreach (var thanks in _specialThanks)
+            {
+                message = message + $"  {count++}. {Context.Client.GetUser(thanks.Key).Username}#{Context.Client.GetUser(thanks.Key).Discriminator} - {thanks.Value}\n";
+            }
+            message = message + $"  {count++}. Everyone from the {Context.Client.GetGuild(255058878268571649)?.Name} clan for giving {Context.Client.GetUser(135556895086870528).Username} the initial idea\n";
+            message = message +"\nI also took heavy inspiration from a fellow bot, Blargbot, who was created by Stupid Cat (thats his username, not me being mean :3)\n" +
+                               "You can find out more about blargbot on its website: <https://blargbot.xyz/>";
             await ReplyAsync(message, ReplyType.Info);
         }
     }
