99e1970e67f02feffaa4379a29f18ef2a1481eba
Stefan Lemvig Frederiksen <stefano_boy_95@hotmail.com>
2017-06-16 17:14:24 +0200

    Added FileUtil test and some restructuring

---
+++ TitanBotTests/Tests/DatabaseTests/TitanBotDbTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TitanBotBase.Database;
+using TitanBotBase.Logger;
+using TitanBotBaseTest.Helpers;
+using TitanBotBaseTest.MockModels;
+
+namespace TitanBotBaseTest.Tests.DatabaseTests
+{
+    [TestClass]
+    public class TitanBotDbTest
+    {
+        [TestMethod]
+        public void CreateDatabase()
+        {
+            const string path = FilesAndFolders.DataBasePath;
+            var mockLogger = new MockLogger();
+
+            var db = new TitanBotDb(path, mockLogger);
+            Console.WriteLine();
+
+            Assert.IsNotNull(db);
+
+            // Cleanup
+            Assert.IsTrue(FilesAndFolders.DeleteTestFolder());
+        }
+    }
+}
---
+++ TitanBotTests/Tests/UtilTests/FileUtilTests.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TitanBotBase.Util;
+using TitanBotBaseTest.Helpers;
+
+namespace TitanBotBaseTest.Tests.UtilTests
+{
+    [TestClass]
+    public class FileUtilTests
+    {
+        [TestMethod]
+        public void EnsureDirectoryCreate()
+        {
+            var path = FilesAndFolders.TestFolderPath;
+            var pathDir = Path.GetDirectoryName(path);
+
+            // Ensure that the file does not exist
+            Assert.IsFalse(Directory.Exists(pathDir));
+
+            FileUtil.EnsureDirectory(path);
+            Assert.IsTrue(Directory.Exists(pathDir));
+
+            // Cleanup
+            Assert.IsTrue(FilesAndFolders.DeleteTestFolder());
+        }
+    }
+}
