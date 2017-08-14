2240e24f43a4895a034005833b2d6d1805e743bd
Titansmasher <dannyjmay97@gmail.com>
2017-08-14 15:01:22 +0100

    oops

--- TitanBot/TypeReaders/Readers/RangeTypeReader.cs
+++ TitanBot/TypeReaders/Readers/RangeTypeReader.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using TitanBot.Contexts;
-using TitanBot.Helpers;
+using TitanBot.Models;
 using static TitanBot.TBLocalisation.Logic;
 
 namespace TitanBot.TypeReaders
