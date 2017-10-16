1b964efd710b7c3a3f1ca9a69294c938e2746e14
Stefan Lemvig Frederiksen <stefano_boy_95@hotmail.com>
2017-10-16 23:38:27 +0200

    Disabled the Remindme command because of a bug in the scheduler and TT2

--- TitanBot/Commands/DefaultCommands/General/RemindmeCommand.cs
+++ TitanBot/Commands/DefaultCommands/General/RemindmeCommand.cs
@@ -6,14 +6,14 @@ using TitanBot.Replying;
 
 namespace TitanBot.Commands.DefaultCommands.General
 {
-    class RemindMeCommand : Command
-    {
-        [Call]
-        [Alias("in")]
-        async Task RemindMeInAsync(TimeSpan duration, [Dense]string message)
-        {
-            Scheduler.Queue<RemindMeHandler>(Author.Id, Guild?.Id, DateTime.Now, null, DateTime.Now.Add(duration), Message.Id, Channel.Id, message);
-            await ReplyAsync(new RawString("Ill remind you in {0}", ReplyType.Success, duration));
-        }
-    }
+    //class RemindMeCommand : Command
+    //{
+    //    [Call]
+    //    [Alias("in")]
+    //    async Task RemindMeInAsync(TimeSpan duration, [Dense]string message)
+    //    {
+    //        Scheduler.Queue<RemindMeHandler>(Author.Id, Guild?.Id, DateTime.Now, null, DateTime.Now.Add(duration), Message.Id, Channel.Id, message);
+    //        await ReplyAsync(new RawString("Ill remind you in {0}", ReplyType.Success, duration));
+    //    }
+    //}
 }
