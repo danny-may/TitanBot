77ee31489499ef5c1193f09896de8a666a378a13
Titansmasher <dannyjmay97@gmail.com>
2017-11-21 00:15:14 +0000

    Added some reply stuff for commands

---
+++ Titanbot/Commands/Models/ReplyMessage.cs
@@ -0,0 +1,28 @@
+﻿using Titansmasher.Services.Display;
+using Titansmasher.Services.Display.Interfaces;
+
+namespace Titanbot.Commands
+{
+    public static class ReplyMessage
+    {
+        #region Statics
+
+        public static readonly string SuccessKey = "replytype.success";
+        public static readonly string InfoKey = "replytype.info";
+        public static readonly string ErrorKey = "replytype.error";
+
+        public static IDisplayable<string> Success(IDisplayable<string> message)
+            => Create(SuccessKey, message);
+
+        public static IDisplayable<string> Error(IDisplayable<string> message)
+            => Create(InfoKey, message);
+
+        public static IDisplayable<string> Info(IDisplayable<string> message)
+            => Create(ErrorKey, message);
+
+        public static IDisplayable<string> Create(string replyTypeKay, IDisplayable<string> message)
+            => new Translation(replyTypeKay, message);
+
+        #endregion Statics
+    }
+}
